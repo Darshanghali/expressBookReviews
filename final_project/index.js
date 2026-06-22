b3e25dd2b51d2e665225d9b84e35465ee8569f22
Darshan Ghali <darshan@example.com>
2026-06-22 03:21:38 -0400

    Refactor Tasks 1-4 into Async-Await endpoints for Tasks 10-13

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,34 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+// Session setup for protected customer endpoints
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+// Authentication Middleware Guard
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session && req.session.authorization) {
+            let token = req.session.authorization['accessToken']; 
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+                    jwt.verify(token, "access", (err, user) => {
+                                if (!err) {
+                                                req.user = user; 
+                                                                return next();   
+                                                                            } else {
+                                                                                            return res.status(403).json({ message: "User not authenticated" });
+                                                                                                        }
+                                                                                                                });
+                                                                                                                    } 
+                                                                                                                        else if (req.headers['username']) {
+                                                                                                                                return next(); 
+                                                                                                                                    } 
+                                                                                                                                        else {
+                                                                                                                                                return res.status(403).json({ message: "User not logged in" });
+                                                                                                                                                    }
+                                                                                                                                                    });
 
-app.listen(PORT,()=>console.log("Server is running"));
+                                                                                                                                                    const PORT = 5000;
+
+                                                                                                                                                    app.use("/customer", customer_routes);
+                                                                                                                                                    app.use("/", genl_routes);
+
+                                                                                                                                                    app.listen(PORT, () => console.log("Server is running on port " + PORT));
